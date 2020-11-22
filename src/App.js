f8dcc2daaca2afa7eda60b08ec500980b0d6646d
Alina Leonova <alinaa.leonova@gmail.com>
2020-11-22 22:44:25 +0200

    task2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import './App.css';
 import Profile from './components/Profile/Profile';
 import user from './data/user.json';
+import Statistics from './components/Statistics/Statistics';
+import statisticalData from './data/statistical-data.json';
 
 
 function App() {
   return (
     <>
       <Profile user={user} />
+      <Statistics title='UPLOAD STATS' stats={statisticalData} />
+
+
     </>
   );
 }
